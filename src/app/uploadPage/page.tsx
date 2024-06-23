1afb272e0da6f5c3b709b889e30fe6a33f64ea69
kelvinComputing <kelvincontrera1@gmail.com>
2024-06-23 17:48:53 -0400

    uploading project

---
+++ src/app/uploadPage/page.tsx
@@ -0,0 +1,49 @@
+import { FC } from "react";
+import Link from 'next/link';
+
+
+const UploadContainer = () => (
+    <div className="upload-container">
+        <input type="file" />
+    </div>
+);
+
+const NextButton = () => (
+    <div className="container">
+      <link rel="stylesheet" href="" />
+      <button className="next-button">NEXT</button>
+    </div>
+);
+
+const BackButton = () => (
+    <div className="container">
+      <Link href="/">
+      <button className="next-button">BACK</button>
+      </Link>
+    </div>
+);
+
+const UploadPage: FC = () => {
+  return (
+    <>
+      <div className="container">
+        <UploadContainer />
+      </div>
+      <div className="containerButton">
+        <BackButton /> 
+        <NextButton />
+      </div>
+    </>
+  );
+};
+
+export default UploadPage;
+
+
+
+
+
+
+
+
+
